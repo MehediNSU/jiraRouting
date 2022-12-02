aa06a5a5aeb503adda5f1578532dd6298c2a5bb8
Md. Mehedi Hasan <mehedibarisal1998@gmail.com>
2022-12-02 22:59:31 +0600

    add save button

--- static/hello-world/src/pages/form.js
+++ static/hello-world/src/pages/form.js
@@ -2,15 +2,23 @@ import { invoke } from '@forge/bridge';
 import { Multiselect } from 'multiselect-react-dropdown';
 import React, { useEffect, useState } from 'react';
 function Form () {
+  var tt ="";
 
 const [projectsData, setProjectsData] = useState([]);
+var [value, setValue] = useState("");
 
   useEffect(() => {
       invoke('getProjectsData').then(data =>{
         let combinedArray = data.map(({name}) => name)
-        setProjectsData(combinedArray)
+        setProjectsData(combinedArray)   
       }) ;
   }, []);
+
+  function test(event){
+    console.log("first",event);
+    setValue(event);
+    console.log("second",value);
+  }
 
 
  return (
@@ -20,9 +28,14 @@ const [projectsData, setProjectsData] = useState([]);
         <Multiselect
         isObject = {false}
         onRemove = {(event)=>console.log(event)}
-        onSelect = {(event)=>console.log(event)}
+        onSelect = {(event)=>test(event)}
          options={projectsData}
           />
+    <div>
+    <form onSubmit={(e) => console.log(e)}>
+        <button type='submit' className='btn btn-primary'>Click me</button>
+      </form>
+    </div>
     </div>
     </div>
 
